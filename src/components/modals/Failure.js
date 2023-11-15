21a1f4c0b68de1287c8a35b8de265c21e04c3bd8
Ali Zaib <alizaib0573@gmail.com>
2023-11-16 00:31:33 +0500

    first commit

--- src/components/modals/Failure.js
+++ src/components/modals/Failure.js
@@ -30,8 +30,8 @@ export default class Failure extends Component {
         return (
             <div ref={this.ui.el}  className="end-screen__modal end-screen__modal--failure">
                 <h1 ref={this.ui.header} className="heading">
-                    <span className='paragraph__line'style={{marginTop:40}} >fading again?</span>
-                    <span className='paragraph__line'>get more energy</span>
+                    <span className='paragraph__line'style={{marginTop:40}} >Need a kick?</span>
+                    {/* <span className='paragraph__line'>get more energy</span> */}
                 </h1>
 
                 {/* <p ref={this.ui.body} className="heading">Give yourself a</p> */}
